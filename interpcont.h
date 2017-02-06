fc9decb1eb035d64e0bb3e691052d490215f13d5
orpheus92 <dwang0127@gmail.com>
2017-02-06 12:16:41 -0700

    Update interpcont.h

--- interpcont.h
+++ interpcont.h
@@ -2,11 +2,9 @@
 #include <Eigen/Dense>
 #include <string>
 #include <cmath>
-#include "opencv2/highgui/highgui.hpp"
 //#include <iostream>
 //#include <stdio.h>
-#include "opencv2/opencv.hpp"
-#include "opencv2/core/core.hpp"
+
 
 //#include "interpcont.cpp"
 
