e021b2602ce84da9bd0f1ce7687d10cf6e29a622
orpheus92 <dwang0127@gmail.com>
2017-02-14 20:59:56 -0700

    Update extForce.cpp

--- extForce.cpp
+++ extForce.cpp
@@ -3,6 +3,11 @@
 //function to calculate the external force
 
 //function to filter an image with a designed filter 
+typedef Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> MatrixXd;
+typedef Eigen::Matrix<double,Eigen::Dynamic,1> VectorXd;
+
+
+MatrixXd compute2(MatrixXd X, MatrixXd W, bool flip, int stride_x, int stride_y);
 void imfilter2(Eigen::MatrixXd src_image,Eigen::MatrixXd filter,Eigen::MatrixXd &dst_image);
 
 //IMGAUSSIAN filters a 2D greyscale image with an Gaussian filter.
@@ -19,20 +24,31 @@ int sigma
 ){ int total = 2*3*sigma+1;
 int row = Img.rows();
 int col = Img.cols();
+
 Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic> x;
-x.resize(total,total);
+x.resize(total,1);
 Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic> y;
-y.resize(total,total);
+y.resize(1,total);
+
 Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic> Gx;
-Gx.resize(total,total);
+Gx.resize(total,1);
 Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic> Gy;
-Gy.resize(total,total);
-Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic> Gxx;
-Gxx.resize(total,total);
-Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic> Gyy;
-Gyy.resize(total,total);
-Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic> Gxy;
-Gxy.resize(total,total);
+Gy.resize(1,total);
+
+for (int i = 0; i< total;i++){
+	//for (int j = 0; j< total;j++){
+//std::cout<<"i = "<<i<<" i-3sig = "<<i-sigma*3<<std::endl;
+		x(i,0) = i-sigma*3;
+		y(0,i) = i-sigma*3;
+}
+
+
+//Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic> Gxx;
+//Gxx.resize(total,total);
+//Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic> Gyy;
+//Gyy.resize(total,total);
+//Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic> Gxy;
+//Gxy.resize(total,total);
 
 
 //Eigen::Matrix<double, total,total> Gx;
@@ -51,6 +67,45 @@ Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic> Ixx;
 Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic> Iyy;
 Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic> Ixy;
 
+
+
+for (int type =0;type<5; type++ )
+{if (type == 0){
+Gx = -(-x.array().square()/2/sigma/sigma).array().exp()*(x.array())/(2*M_PI*pow(sigma,4));
+Gy = (-y.array().square()/2/sigma/sigma).array().exp();
+Ix = compute2(Img, Gx, false, 1, 1);
+Ix = compute2(Ix, Gy, false, 1, 1);
+}
+//G(i,j)=-x(i,j)/(2*M_PI*pow(sigma,4))*exp(-(pow(x(i,j),2)+pow(y(i,j),2))/(2*pow(sigma,2)));
+else if (type == 1){
+Gy = -(-y.array().square()/2/sigma/sigma).array().exp()*(y.array())/(2*M_PI*pow(sigma,4));
+Gx = (-x.array().square()/2/sigma/sigma).array().exp();
+Iy = compute2(Img, Gx, false, 1, 1);
+Iy = compute2(Iy, Gy, false, 1, 1);
+}
+//G(i,j)=-y(i,j)/(2*M_PI*pow(sigma,4))*exp(-(pow(x(i,j),2)+pow(y(i,j),2))/(2*pow(sigma,2)));
+else if (type == 2){
+Gx = (-x.array().square()/2/sigma/sigma).array().exp()*(x.array().square()/sigma/sigma-1)/(2*M_PI*pow(sigma,4));
+Gy = (-y.array().square()/2/sigma/sigma).array().exp();
+Ixx = compute2(Img, Gx, false, 1, 1);
+Ixx = compute2(Ixx, Gy, false, 1, 1);
+}
+//G(i,j)=1/(2*M_PI*pow(sigma,4))*(pow(x(i,j),2)/pow(sigma,2)-1) *exp(-(pow(x(i,j),2)+pow(y(i,j),2))/(2*pow(sigma,2)));
+else if (type == 3){
+Gy = (-y.array().square()/2/sigma/sigma).array().exp()*(y.array().square()/sigma/sigma-1)/(2*M_PI*pow(sigma,4));
+Gx = (-x.array().square()/2/sigma/sigma).array().exp();
+Iyy = compute2(Img, Gx, false, 1, 1);
+Iyy = compute2(Iyy, Gy, false, 1, 1);
+}
+//G(i,j)=1/(2*M_PI*pow(sigma,4))*(pow(y(i,j),2)/pow(sigma,2)-1) *exp(-(pow(x(i,j),2)+pow(y(i,j),2))/(2*pow(sigma,2)));
+else {
+Gx = (-x.array().square()/2/sigma/sigma).array().exp()*(x.array())/(2*M_PI*pow(sigma,6));
+Gy = (-y.array().square()/2/sigma/sigma).array().exp()*(y.array());
+Ixy = compute2(Img, Gx, false, 1, 1);
+Ixy = compute2(Ixy, Gy, false, 1, 1);
+}
+}
+/*
 for (int i = 0; i< total;i++){
 	for (int j = 0; j< total;j++){
 		x(i,j) = i-sigma*3;
@@ -67,22 +122,33 @@ for (int i = 0; i< total;i++){
         Gxy(i,j)=1/(2*M_PI*pow(sigma,6))*(x(i,j)*y(i,j)) *exp(-(pow(x(i,j),2)+pow(y(i,j),2))/(2*pow(sigma,2)));
 
 }//end for j
-}//end for i
+}*/
+//end for i
+//std::cout<<"x = "<<x<<std::endl;
+//std::cout<<"y = "<<y<<std::endl;
 //Gaussian Based Image Derivatives
 
 
 //static void 	cv::eigen2cv (const Eigen::Matrix< _Tp, _rows, _cols, _options, _maxRows, _maxCols > &src, Mat &dst)
 //cv::eigen2cv (Img, Mat &dst);
-imfilter2(Img, Gx, Ix);
-imfilter2(Img, Gy, Iy);
-imfilter2(Img, Gxx, Ixx);
-imfilter2(Img, Gyy, Iyy);
-imfilter2(Img, Gxy, Ixy);
+//std::cout<<"old Gx"<<Gx<<std::endl;
+//std::cout<<"old Gy"<<Gy<<std::endl;
+
+
+//imfilter2(Img, Gx, Ix);
+//imfilter2(Img, Gy, Iy);
+//imfilter2(Img, Gxx, Ixx);
+//imfilter2(Img, Gyy, Iyy);
+//imfilter2(Img, Gxy, Ixy);
+//std::cout<<"Img = "<<Img<<std::endl;
+//std::cout<<"Ix = "<<Ix<<std::endl;
+//std::cout<<"Iyy = "<<Iyy<<std::endl;
+//std::cout<<"Iy = "<<Iy<<std::endl;
 
 //Line Energy
 Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic>Eline;
 Eline = imgaussian(Img,sigma);
-
+//std::cout<<"Eline = "<<Eline<<std::endl;
 //std::cout<<Iyy.cwiseProduct(Ix2) <<std::endl;
 //Line Termination Energy
 Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic>Eterm;
@@ -102,7 +168,7 @@ Eterm = nom.cwiseQuotient(denom);
 
 Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic>Eedge;
 Eedge = (Ix.array().square() + Iy.array().square()).array().sqrt(); 
-
+//std::cout<<"Eedge = "<<Eedge<<std::endl;
 //Externa =l Energy
 Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic>Eext;
 
@@ -119,7 +185,7 @@ void imfilter2(Eigen::MatrixXd src_image, Eigen::MatrixXd filter,Eigen::MatrixXd
 	
 	int start_row = int(filter.rows()/2) ;
 	int start_col = int(filter.cols()/2) ;
-
+	//std::cout<<"start_row = "<<start_row<<std::endl;
 	Eigen::MatrixXd Mid_Matrix = Eigen::MatrixXd::Zero(src_image.rows()+2*start_row,src_image.cols()+2*start_col) ;
 
 	for (int i = 0; i < src_image.rows(); i++)
@@ -146,7 +212,7 @@ void imfilter2(Eigen::MatrixXd src_image, Eigen::MatrixXd filter,Eigen::MatrixXd
 			{				
 				for (int n = 0; n < filter_col; n++)
 				{
-					dst_image(tmp_row,tmp_col) += Mid_Matrix(tmp_row + m,tmp_col + n)*filter(m,n) ; 
+					dst_image(tmp_row,tmp_col) -= Mid_Matrix(tmp_row + m,tmp_col + n)*filter(m,n) ; 
 				}
 			}
 		}
@@ -187,5 +253,44 @@ return I;
 }
 
 
+MatrixXd compute2(MatrixXd X, MatrixXd W, bool flip, int stride_x, int stride_y)
+	{
+	int width = X.cols();
+	int height = X.rows();
+	int kx = W.cols();
+	int ky = W.rows();
+	int rx = (kx-1)/2;
+	int ry = (ky-1)/2;
+	MatrixXd out;
+	out.resize(height, width);
+           for (int x=0; x<width; x+=stride_x)
+           {
+               int xout = x/stride_x;
+   
+               for (int y=0; y<height; y+=stride_y)
+               {
+                   int yout = y/stride_y;
+   
+                   double sum = 0;
+
+		           for (int x1=x-rx; x1<=x+rx; x1++)
+		           {
+		               int wx = flip ? x1-x+rx : rx-x1+x;
+		               for (int y1=y-ry; y1<=y+ry; y1++)
+		               {
+		                   if (x1>=0 && y1>=0 && x1<width && y1<height)
+		                   {
+		                       if (flip)
+		                           sum += W(y1-y+ry,wx)*X(y1,x1);
+		                       else
+		                           sum += W(ry-y1+y,wx)*X(y1,x1);
+		                   }
+		               }
+		           }
+                   out(yout,xout) = sum;
+               }
+           }
+	return out;       
+	}
 
 
